52e20d889cf69696188310bed59642614f7259c2
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2018-06-10 16:42:01 -0400

    parte del chat completado

--- app/src/main/java/com/gareca/orlando/messenger/Mensajes/MensajesAdapter.java
+++ app/src/main/java/com/gareca/orlando/messenger/Mensajes/MensajesAdapter.java
@@ -1,11 +1,18 @@
 package com.gareca.orlando.messenger.Mensajes;
 
+import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.gareca.orlando.messenger.R;
@@ -15,9 +22,11 @@ import java.util.List;
 public class MensajesAdapter extends RecyclerView.Adapter<MensajesAdapter.MensajesViewHolder>{
 
     private List<MensajeDeTexto> mensajesDeTextos;
+    private Context context;
 
-    public MensajesAdapter(List<MensajeDeTexto> mensajesDeTextos) {
+    public MensajesAdapter(List<MensajeDeTexto> mensajesDeTextos,Context context) {
         this.mensajesDeTextos = mensajesDeTextos;
+        this.context = context;
     }
 
     @NonNull
@@ -29,8 +38,40 @@ public class MensajesAdapter extends RecyclerView.Adapter<MensajesAdapter.Mensaj
 
     @Override
     public void onBindViewHolder(@NonNull MensajesViewHolder holder, int position) {
+        RelativeLayout.LayoutParams rl = (RelativeLayout.LayoutParams) holder.carView.getLayoutParams();
+        FrameLayout.LayoutParams fl = (FrameLayout.LayoutParams) holder.mensajeBG.getLayoutParams();
+        LinearLayout.LayoutParams llHora = (LinearLayout.LayoutParams)holder.TvHora.getLayoutParams();
+        LinearLayout.LayoutParams llMensaje = (LinearLayout.LayoutParams)holder.TvMensaje.getLayoutParams();
+        if(mensajesDeTextos.get(position).getTipoMensaje()==1){//emisor
+            holder.mensajeBG.setBackgroundResource(R.drawable.in_message_bg);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_LEFT,0);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            llMensaje.gravity = Gravity.RIGHT;
+            llHora.gravity = Gravity.RIGHT;
+            fl.gravity = Gravity.RIGHT;
+            holder.TvMensaje.setGravity(Gravity.RIGHT);
+        }else if(mensajesDeTextos.get(position).getTipoMensaje()==2){
+            holder.mensajeBG.setBackgroundResource(R.drawable.out_message_bg);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,0);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            fl.gravity = Gravity.LEFT;
+            llMensaje.gravity = Gravity.LEFT;
+            llHora.gravity = Gravity.LEFT;
+            holder.TvMensaje.setGravity(Gravity.LEFT);
+        }
+        holder.carView.setLayoutParams(rl);
+        holder.mensajeBG.setLayoutParams(fl);
+        holder.TvHora.setLayoutParams(llHora);
+        holder.TvMensaje.setLayoutParams(llMensaje);
+
         holder.TvMensaje.setText(mensajesDeTextos.get(position).getMensaje());
         holder.TvHora.setText(mensajesDeTextos.get(position).getHoraDelMensaje());
+        if(android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            holder.carView.getBackground().setAlpha(0);
+        }else{
+            holder.carView.setBackgroundColor(ContextCompat.getColor(context,android.R.color.transparent));
+        }
+
     }
 
     @Override
@@ -41,11 +82,13 @@ public class MensajesAdapter extends RecyclerView.Adapter<MensajesAdapter.Mensaj
     static class MensajesViewHolder extends RecyclerView.ViewHolder{
 
         CardView carView;
+        LinearLayout mensajeBG;
         TextView TvMensaje;
         TextView TvHora;
         MensajesViewHolder(View itemView) {
             super(itemView);
             carView = (CardView) itemView.findViewById(R.id.cvMensaje);
+            mensajeBG = (LinearLayout)itemView.findViewById(R.id.mensajeBG);
             TvMensaje = (TextView) itemView.findViewById(R.id.msTexto);
             TvHora = (TextView) itemView.findViewById(R.id.msHora);
 
--- app/src/main/java/com/gareca/orlando/messenger/Mensajes/mensajeria.java
+++ app/src/main/java/com/gareca/orlando/messenger/Mensajes/mensajeria.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gareca.orlando.messenger.R;
 
@@ -18,15 +23,19 @@ import java.util.List;
 public class mensajeria extends AppCompatActivity {
 
     private RecyclerView rv;
+    private Button bTenviarMensaje;
+    private EditText eTEscribirMensaje;
     private List<MensajeDeTexto> mensajeDeTextos;
     private MensajesAdapter adapter;
+    private int TEXT_LINES = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensajeria);
         mensajeDeTextos = new ArrayList<>();
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-
+        bTenviarMensaje = (Button)findViewById(R.id.bTEnviarMensaje);
+        eTEscribirMensaje = (EditText)findViewById(R.id.eTEscribirMensaje);
         rv = (RecyclerView)findViewById(R.id.rvMensajes);
         LinearLayoutManager lm = new LinearLayoutManager(this);
         rv.setLayoutManager(lm);
@@ -34,15 +43,49 @@ public class mensajeria extends AppCompatActivity {
         for(int i=0;i<10;i++){
             MensajeDeTexto mensajeDeTextoAuxiliar = new MensajeDeTexto();
             mensajeDeTextoAuxiliar.setId(""+i);
-            mensajeDeTextoAuxiliar.setMensaje("hola"+i);
-            mensajeDeTextoAuxiliar.setTipoMensaje(i);
+            mensajeDeTextoAuxiliar.setMensaje("El emisor numero"+i + "Te ha enviado un mensaje");
+            mensajeDeTextoAuxiliar.setTipoMensaje(1);
             mensajeDeTextoAuxiliar.setHoraDelMensaje("10:2"+i);
             mensajeDeTextos.add(mensajeDeTextoAuxiliar);
         }
 
-        adapter = new MensajesAdapter(mensajeDeTextos);
+        for(int i=0;i<10;i++){
+            MensajeDeTexto mensajeDeTextoAuxiliar = new MensajeDeTexto();
+            mensajeDeTextoAuxiliar.setId(""+i);
+            mensajeDeTextoAuxiliar.setMensaje("el Receptor numero"+i+ "te ah enviado un mensaje");
+            mensajeDeTextoAuxiliar.setTipoMensaje(2);
+            mensajeDeTextoAuxiliar.setHoraDelMensaje("10:2"+i);
+            mensajeDeTextos.add(mensajeDeTextoAuxiliar);
+        }
+
+        adapter = new MensajesAdapter(mensajeDeTextos,this);
         rv.setAdapter(adapter);
 
+        eTEscribirMensaje.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(eTEscribirMensaje.getLayout().getLineCount()!=TEXT_LINES){
+                    setScrollBarChat();
+                    TEXT_LINES = eTEscribirMensaje.getLayout().getLineCount();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        bTenviarMensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateMensaje(eTEscribirMensaje.getText().toString());
+            }
+        });
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +93,20 @@ public class mensajeria extends AppCompatActivity {
                 finish();
             }
         });
+        setScrollBarChat();
+    }
+    public void CreateMensaje(String mensaje){
+        MensajeDeTexto mensajeDeTextoAuxiliar = new MensajeDeTexto();
+        mensajeDeTextoAuxiliar.setId("0");
+        mensajeDeTextoAuxiliar.setMensaje(mensaje);
+        mensajeDeTextoAuxiliar.setTipoMensaje(1);
+        mensajeDeTextoAuxiliar.setHoraDelMensaje("10:20");
+        mensajeDeTextos.add(mensajeDeTextoAuxiliar);
+        adapter.notifyDataSetChanged();
+        eTEscribirMensaje.setText("");
+        setScrollBarChat();
+    }
+    public void setScrollBarChat(){
+        rv.scrollToPosition(adapter.getItemCount()-1);
     }
 }
